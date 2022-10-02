03f8969388290232354950487e8887a8a4718e78
Yara <hybridme2kxp@gmail.com>
2022-10-03 02:39:16 +0700

    dz01 ex02 added

---
+++ dz_cs/example02/Program.cs
@@ -0,0 +1,11 @@
+﻿// Задача 2: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+Console.WriteLine("Введите три любых числа:");
+int number1 = Convert.ToInt32(Console.ReadLine());
+int number2 = Convert.ToInt32(Console.ReadLine());
+int number3 = Convert.ToInt32(Console.ReadLine());
+if (number1>number2 & number1>number3)
+{System.Console.WriteLine($"Число {number1} - самое большое");}
+if (number2>number1 & number2>number3)
+{System.Console.WriteLine($"Число {number2} - самое большое");}
+if (number3>number2 & number3>number1)
+{System.Console.WriteLine($"Число {number3} - самое большое");}
