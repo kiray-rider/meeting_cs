9a39855057a49bfd47c4d035da48f234a91850c8
Yara <hybridme2kxp@gmail.com>
2022-10-11 01:06:52 +0700

    dz01 ex02 added teacher variant

--- dz_cs01/example02/Program.cs
+++ dz_cs01/example02/Program.cs
@@ -1,5 +1,7 @@
 ﻿// Задача 2: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
-Console.WriteLine("Введите три любых числа:");
+
+//______________Мой вариант:___________________
+/* Console.WriteLine("Введите три любых числа:");
 int number1 = Convert.ToInt32(Console.ReadLine());
 int number2 = Convert.ToInt32(Console.ReadLine());
 int number3 = Convert.ToInt32(Console.ReadLine());
@@ -8,4 +10,21 @@ if (number1>number2 & number1>number3)
 if (number2>number1 & number2>number3)
 {System.Console.WriteLine($"Число {number2} - самое большое");}
 if (number3>number2 & number3>number1)
-{System.Console.WriteLine($"Число {number3} - самое большое");}
+{System.Console.WriteLine($"Число {number3} - самое большое");} */
+
+//____________Вариант преподавателя_____________
+// Такой алгоритм лучше:
+Console.WriteLine("Введите три любых числа:");
+int number1 = Convert.ToInt32(Console.ReadLine());
+int number2 = Convert.ToInt32(Console.ReadLine());
+int number3 = Convert.ToInt32(Console.ReadLine());
+int max = number1;
+if (max < number2)
+{
+    max = number2;
+}
+if (max < number3)
+{
+    max = number3;
+}
+System.Console.WriteLine($"Число {max} - самое большое");
