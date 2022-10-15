7ed59c9614c5eeb4aed917cb1a6b13ee230beab9
Yara <hybridme2kxp@gmail.com>
2022-10-16 02:06:58 +0700

    dz03 ex03 added (cube all int to N)

---
+++ dz_cs03/example03_cube_all_int/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задача 3. Напишите программу, которая принимает на вход 
+// число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+Console.Write("Введите число N: ");
+int numberN = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Таблица кубов чисел, от 1 до N: ");
+for (int number = 1; number <= numberN; number++) // для всех чисел от 1 до N, с прибавлением шага на +1, выполнять:
+{
+    Console.Write($"{Math.Pow(number,3)}, "); // возведение в куб
+    if (number % 10 == 0) Console.WriteLine();   // с разбивкой по 10 чисел в строке
+}
