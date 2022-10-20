68666cd02561cfab3c10a608dbca32d8d32184c8
Yara <hybridme2kxp@gmail.com>
2022-10-20 11:12:08 +0700

    dz04 ex02 added (summ of digits in number)

---
+++ dz_cs04/ex02_digsumm/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 2: Напишите программу, которая принимает на вход число 
+// и выдаёт сумму цифр в числе. Функция должна на вход принимать 
+// число, а выдавать сумму его цифр
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+int num = InputNumber("Введите число: ");
+int dSum = DigSumm(num);
+Console.WriteLine($"Сумма цифр этого числа равна = {dSum}");
+
+int InputNumber(string message)
+{
+    Console.Write(message);
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+
+int DigSumm(int num)
+{
+    int dSum = 0;
+    while(num > 0)
+    {
+    dSum = num % 10 + dSum;
+    num = (num - num % 10) / 10;
+    }
+    return dSum;
+}
