db9e66adbecf1da2f95b724ffd0128ed7551ec47
Yara <hybridme2kxp@gmail.com>
2022-12-09 15:10:18 +0700

    dz09 ex03 added (Akkerman function)

---
+++ dz_cs09_end/ex03_akkerman_func/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача 3: Напишите программу вычисления функции Аккермана 
+// с помощью рекурсии. Даны два неотрицательных числа m и n.
+// m = 3, n = 2 -> A(m,n) = 29
+// m = 1, n = 2 -> A(m,n) = 4
+// m = 0, n = 1 -> A(m,n) = 2
+// m = 0, n = 0 -> A(m,n) = 1
+// m = 2, n = 2 -> A(m,n) = 7
+
+Console.WriteLine("\nКомпьютер посчитает функцию Аккермана А(m,n). \nДаны два неотрицательных числа m и n");
+int m = InputValue("Введите число m: ");
+int n = InputValue("Введите число n: ");
+AkkermanCheck(m, n);
+
+int InputValue(string text)
+{
+    Console.Write(text);
+    int value = Convert.ToInt32(Console.ReadLine());
+    return value;
+}
+
+void AkkermanCheck(int m, int n)
+{
+if (m < 0 || n < 0) Console.WriteLine($"Так не пойдет! числа не должны быть отрицательными!");
+else Console.WriteLine($"Результат функции: {Akkerman(m,n)}");
+}
+
+int Akkerman(int m, int n)
+{
+    if (m == 0) return n + 1;
+    else if ((m > 0) && (n == 0)) return Akkerman(m - 1, 1);
+    else return Akkerman(m - 1, Akkerman(m, n - 1));
+}
